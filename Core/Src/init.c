64113bc0bda48767639fa044e6ada8c57ede849a
Eugene <142393747+Lckb@users.noreply.github.com>
2024-12-30 00:38:29 +0300

    MAIN FILES CREATED

---
+++ Core/Src/init.c
@@ -0,0 +1,11 @@
+ #include<stdint.h>
+
+void GPIO_Init(void){
+    *(uint32_t*)(0x40023800UL + 0x30UL) |= 0x02;
+    *(uint32_t*)(0x40020400UL + 0x00UL) |= 0x4000;
+    *(uint32_t*)(0x40020400UL + 0x08UL) |= 0x4000;
+    *(uint32_t*)(0x40020400UL + 0x0CUL) |= 0x00;
+    if((*(uint32_t*)(0x40020400UL + 0x10UL) & 0x80UL) != 0);
+    *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x800000; // bit reset
+    *(uint32_t*)(0x40020400UL + 0x18UL) |= 0x80; // bit set
+}
